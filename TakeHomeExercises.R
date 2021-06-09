44f88bce3b43de63915c71369c2dc46fecc30ec3
noaharagon <50531441+noaharagon@users.noreply.github.com>
2021-06-09 20:30:11 +0200

    Update TakeHomeExercises.R
    Task 1

--- TakeHomeExercises.R
+++ TakeHomeExercises.R
@@ -10,13 +10,20 @@ library(rvest)
 library(dplyr)
 library(readr)
 library(vroom)
+library(parallel)
+library(R.utils)
+library(data.table)
 
 # Task 1 ------------------------------------------------------------------
-setwd("/Users/noahangara/Documents/GitHub/take-home-exercises-team-send-it/data")
+# set working directory according to who is executing the code
+Paths = c("/Users/jonasschmitten/Desktop/GitHub/BigDataWSB",
+          "/Users/noahangara/Documents/GitHub/take-home-exercises-team-send-it/data")
+names(Paths) = c("jonasschmitten", "noahangara")
+setwd(Paths[Sys.info()[7]])
 
 #get HTML links 
 fec_page <- read_html("https://sunlightlabs.github.io/datacommons/bulk_data.html")
-zip_links <- fec_page %>% #get all links for the zip files
+zip_links <- fec_page %>% #get all links for the zip files by contribution year
   html_nodes(xpath="//*[@id='content']/ul[1]") %>% html_nodes("a") %>%
   html_attr("href")
 
@@ -25,24 +32,25 @@ getOption("timeout")
 options(timeout = 1000)
 
 #download zip files
-#RUNTIME : (depends on network speed)
+#RUNTIME : ~9 minutes (depends on network speed)
 download.file(zip_links, basename(zip_links), method="libcurl")
-
-#unzip and remove zip fies
-sapply(basename(zip_links), function(x)unzip(x, exdir = getwd()))
-file.remove(c(basename(zip_links), "README"))
+#unzip and remove zip files
+sapply(basename(zip_links), function(x)gunzip(x))
 gc()
 
 #combine csv files into one (only reading one file into memory at a time)
-#RUNTIME: 
-for (i in list.files(pattern = ".csv")) {
-  d <- vroom(i, num_threads = 4)
+#RUNTIME: ~7.83 minutes
+beginning <- Sys.time()
+files <- list.files(pattern = ".csv")
+for (i in files) {
+  d <- vroom(i, num_threads = detectCores())
   gc()
   first <- i == list.files(pattern = ".csv")[1]
-  vroom_write(d, "fec.csv", num_threads = 4, append = !first)
+  fwrite(d, "fec.csv", nThread = detectCores(), append = !first)
 }
+ending <- Sys.time()
 
 #compress csv file and remove redundant files
-zip(zipfile = "fec.csv.zip", files = "fec.csv")
-file.remove(list.files(pattern = ".csv$"))
-
+#RUNTIME: ~3.5 minutes
+gzip("fec.csv", destname = "fec.csv.zip")
+file.remove(list.files(pattern = "contributions"))
