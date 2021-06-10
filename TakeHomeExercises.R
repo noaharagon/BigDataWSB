ba5cceb1318905e40a9a1a4ffe811fe69513a799
noaharagon <50531441+noaharagon@users.noreply.github.com>
2021-06-10 14:24:22 +0200

    Update TakeHomeExercises.R

--- TakeHomeExercises.R
+++ TakeHomeExercises.R
@@ -32,7 +32,7 @@ getOption("timeout")
 options(timeout = 1000)
 
 #download zip files
-#RUNTIME : ~9 minutes (depends on network speed)
+#RUNTIME : ~9 minutes (depends on network speed) on Macbook Air 2017 i7 8GB RAM
 download.file(zip_links, basename(zip_links), method="libcurl")
 #unzip and remove zip files
 lapply(basename(zip_links), function(x)unzip(x, exdir = getwd()))
@@ -40,19 +40,19 @@ file.remove(c(basename(zip_links), "README"))
 gc()
 
 #combine csv files into one (only reading one file into memory at a time)
-#RUNTIME: ~7.83 minutes
+#RUNTIME: ~7.29 minutes on Macbook Air 2017 i7 8GB RAM
 beginning <- Sys.time()
 files <- list.files(pattern = ".csv$")
 for (i in files) {
-  d <- vroom(i, num_threads = detectCores())
+  d <- vroom(i, num_threads = detectCores(), na = c("", "NA", " "))
   gc()
-  first <- i == list.files(pattern = ".csv")[1]
-  fwrite(d, "fec.csv", nThread = detectCores(), append = !first)
+  first <- i == list.files(pattern = "contribution")[1]
+  fwrite(d, "fec.csv", nThread = detectCores(), append = !first, na = c("", "NA", " "))
 }
 ending <- Sys.time()
 
 #compress csv file and remove redundant files
-#RUNTIME: ~3.5 minutes
+#RUNTIME: ~3.5 minutes on Macbook Air 2017 i7 8GB RAM
 gzip("fec.csv", destname = "fec.csv.zip")
 file.remove(list.files(pattern = "contributions"))
 
@@ -97,4 +97,4 @@ dbWriteTable(con, "industrycodes", industrycodes, field.types = c(
   order = "varchar(3)"))
 
 #Create table for donations"
-
+dbWriteTable(con, "donations", "fec.csv")
